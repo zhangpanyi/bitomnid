f9ef73d96d312d71fb4e8dab614c731c7216a6ed
zpy <zhangpanyi@live.com>
2019-07-27 21:19:29 +0800

    update

---
+++ app/poller.js
@@ -0,0 +1,12 @@
+class Poller {
+    constructor(client) {
+        this._client = client;
+    }
+
+    // 开始轮询
+    async startPolling() {
+        
+    }
+}
+
+module.exports = Poller;
--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const Client = require('bitcoin-core');
 
 const server = require('./config/server');
 
+const Poller = require('./app/poller');
 const RPCServer = require('./app/rpcserver');
+
 const logger = require('./app/common/logger');
 
 try {
     const client = new Client(server.endpoint);
 
-    let collect = require('./app/handlers/collect');
-    collect(client, '1');
-    // const rpcserver = new RPCServer(client);
-    // rpcserver.start();
+    const poller = new Poller(client);
+    poller.startPolling();
+    
+    const rpcserver = new RPCServer(client);
+    rpcserver.start();
 } catch (error) {
     logger.fatal('Service terminated, reason: %s', error.message)
 }
