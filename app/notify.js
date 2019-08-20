54489cdd30027ea08705616028cac009a0f0cee6
zpy <zhangpanyi@live.com>
2019-08-20 20:13:28 +0800

    update

--- app/notify.js
+++ app/notify.js
@@ -9,25 +9,24 @@ function Notify() {
     let amount      = '0';  // 转账金额
 
     // 投递通知
-    this.post = function(url) {
-        if (!url) {
-            return;
-        }
+    this.post = function(urls) {
         this.type = 'transaction';
         let data = JSON.stringify(this);
-        let options = {
-            url: url,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: data
-        };
-        request.post(options, function (error, response, body) {
-            if (error != null) {
-                logger.error('Failed to post notify: %s, %s', error.message, options.json);
-            }
-        });
+        for (let idx = 0; idx < urls.length; idx++) {
+            let options = {
+                url: urls[idx],
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: data
+            };
+            request.post(options, function (error, response, body) {
+                if (error != null) {
+                    logger.error('Failed to post notify: %s, %s', error.message, options.json);
+                }
+            });
+        }
     }
 };
 
